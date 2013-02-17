b33e48c86f61869397a337b955fe4d848e4f677f
michael99man <michael99man.bj@gmail.com>
2013-02-17 19:53:44 +0800

    Added YOLOSWAG
    Added YOLOSWAG

--- src/com/github/assisstion/spaceInvaders/Test.java
+++ src/com/github/assisstion/spaceInvaders/Test.java
@@ -3,5 +3,8 @@ package com.github.assisstion.spaceInvaders;
 public class Test{
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+		System.out.println("YOLOSWAG");
+		//prints YOLOSWAG
+		
 	}
 }
