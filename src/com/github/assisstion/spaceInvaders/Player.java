da610b21b53638fe7f3ebfa93f15211c2139d0aa
Markus Feng <markusfeng@gmail.com>
2013-02-19 19:02:15 +0800

    Fixed Player Class to support Image functionality
    Calls the superclass constructor to automatically set it up. Note that
    there is no current player image location, so this will not currently
    work. Please add Player image location ASAP.

--- src/com/github/assisstion/spaceInvaders/Player.java
+++ src/com/github/assisstion/spaceInvaders/Player.java
@@ -1,18 +1,22 @@
 package com.github.assisstion.spaceInvaders;
 
-import java.awt.Image;
 import java.io.IOException;
 
 public class Player extends Sprite {
+	
+	private static final String PLAYER_DEFAULT_IMAGE = null;
 
 	//Constructor
-	public Player(String name){
+	public Player(String name) throws IOException{
+		//Calls the superclass constructor to automatically set up the player image
+		super(PLAYER_DEFAULT_IMAGE);
 		this.name=name;
+		
 	}
 	
 	private String name;
-	private int score;
-	private Image playerSprite;
+	//Made this public so other classes can easily access it
+	public int score;
 	public Direction currentDirection=null;
 	
 	public static enum Direction {
