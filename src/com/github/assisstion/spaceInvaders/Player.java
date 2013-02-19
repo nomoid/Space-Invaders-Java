f013ef021ad1ca258a50413bd6d5d3f70e1f8a72
michael99man <michael99man.bj@gmail.com>
2013-02-19 17:56:13 +0800

    Added Player Class

---
+++ src/com/github/assisstion/spaceInvaders/Player.java
@@ -0,0 +1,21 @@
+package com.github.assisstion.spaceInvaders;
+
+import java.awt.Image;
+import java.io.IOException;
+
+public class Player extends Sprite {
+
+
+	public Player(String imageLink) throws IOException {
+		super(imageLink);
+		// TODO Auto-generated constructor stub
+	}
+	
+	private String name;
+	private int score;
+	private Image playerSprite;
+	
+
+	
+
+}
