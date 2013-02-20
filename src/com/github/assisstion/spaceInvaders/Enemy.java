848a9c00d19c092ff2e70a39ea94063fabf49ee7
Markus Feng <markusfeng@gmail.com>
2013-02-20 20:58:52 +0800

    Enemy class skeleton created

---
+++ src/com/github/assisstion/spaceInvaders/Enemy.java
@@ -0,0 +1,23 @@
+package com.github.assisstion.spaceInvaders;
+
+import java.io.IOException;
+
+public class Enemy extends Sprite{
+	
+	private static final int[] ENEMY_HEALTH = {};
+	private static final String[] ENEMY_IMAGE = {};
+	
+	public int health;
+	public int xUpdateCounter;
+	public int yUpdateCounter;
+	
+	public Enemy(EnemyType type) throws IOException{
+		super(ENEMY_IMAGE[type.ordinal()]);
+		int index = type.ordinal();
+		health = ENEMY_HEALTH[index];
+	}
+	
+	public static enum EnemyType{
+		
+	}
+}
