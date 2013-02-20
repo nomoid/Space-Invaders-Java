fbf30627fdbb03a245720b2c6be97af9ecf446d1
Markus Feng <markusfeng@gmail.com>
2013-02-20 21:14:44 +0800

    Fixed Naming Convensions and added to Bunker class
    Removed some duplicate or unneeded variables

--- src/com/github/assisstion/spaceInvaders/Bunker.java
+++ src/com/github/assisstion/spaceInvaders/Bunker.java
@@ -1,26 +1,22 @@
 package com.github.assisstion.spaceInvaders;
 
-import java.awt.Point;
 import java.io.IOException;
 
 public class Bunker extends Sprite {
-		private static final String PLAYER_DEFAULT_IMAGE = "resources/placeholder.jpg";
 		
 		//normal square
-		private static final String BunkerA = "resources/placeholder.jpg";
+		private static final String BUNKER_A = "resources/placeholder.jpg";
 		//curved side to the left 
-		private static final String BunkerB = "resources/placeholder.jpg";
+		private static final String BUNKER_B = "resources/placeholder.jpg";
 		//curved side to the right
-		private static final String BunkerC = "resources/placeholder.jpg";
+		private static final String BUNKER_C = "resources/placeholder.jpg";
 		//curved side to the bottom right 
-		private static final String BunkerD = "resources/placeholder.jpg";
+		private static final String BUNKER_D = "resources/placeholder.jpg";
 		//curved side to the bottom left
-		private static final String BunkerE = "resources/placeholder.jpg";
+		private static final String BUNKER_E = "resources/placeholder.jpg";
 		
+		private int bunkerNum;
 		
-		private int BunkerNum;
-		private int BunkerX;
-		private int BunkerY;
 		protected Bunker(){
 		
 			
@@ -28,11 +24,31 @@ public class Bunker extends Sprite {
 		
 		//Constructor
 		//Asks for BunkerID, The X value of the Bunker and the Y value. Implementation will come later.
-		public Bunker(int BunkerNum, int BunkerX, int BunkerY) throws IOException{
-			//Calls the superclass constructor to automatically set up the player image
-			super(PLAYER_DEFAULT_IMAGE);
-			this.BunkerNum=BunkerNum;
-			
+		public Bunker(int bunkerNum, int bunkerX, int bunkerY) throws IOException{
+			//Calls the superclass constructor to set up the image for THIS OBJECT
+			super(getImageLinkFromBunkerNumber(bunkerNum), bunkerX, bunkerY);
+			this.bunkerNum=bunkerNum;
+		}
+		
+		public static String getImageLinkFromBunkerNumber(int bunkerNum){
+			switch(bunkerNum){
+				case 0:
+					return BUNKER_A;
+				case 1:
+					return BUNKER_B;
+				case 2:
+					return BUNKER_C;
+				case 3:
+					return BUNKER_D;
+				case 4:
+					return BUNKER_E;
+				default:
+					throw new IllegalArgumentException("Illegal Bunker Number");
+			}
+		}
+		
+		public int getBunkerNum(){
+			return bunkerNum;
 		}
 
 }
