c678143bb7c125f33d7ddd85fea1c971b7a277e4
Markus Feng <markusfeng@gmail.com>
2013-03-16 15:48:00 +0800

    Started tabbed pane support in high scores

--- src/com/github/assisstion/spaceInvaders/menu/HighScoreMenuBuilder.java
+++ src/com/github/assisstion/spaceInvaders/menu/HighScoreMenuBuilder.java
@@ -1,16 +1,34 @@
 package com.github.assisstion.spaceInvaders.menu;
 
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+
+import com.github.assisstion.spaceInvaders.MainCanvas;
+
 public class HighScoreMenuBuilder implements MenuBuilder{
-//will be made later
+
+	private JTabbedPane tabbedPane;
+	private Menu parent;
+	
 	@Override
 	public void build(Menu menu) {
-		// TODO Auto-generated method stub
-		
+		parent = menu;
+		tabbedPane = new JTabbedPane();
+		tabbedPane.setBounds(10, 10, MainCanvas.FRAME_WIDTH - 20, MainCanvas.FRAME_HEIGHT - 20);
+		JLabel label = new JLabel("hi");
+		label.setForeground(Color.WHITE);
+		tabbedPane.insertTab("hi", null, label, "epic", 0);
+		JLabel label2 = new JLabel("bye");
+		label2.setForeground(Color.WHITE);
+		tabbedPane.insertTab("bye", null, label2, "moreEpic", 0);
+		parent.add(tabbedPane);
 	}
 
 	@Override
 	public void unBuild(Menu menu) {
-		// TODO Auto-generated method stub
+		parent.remove(tabbedPane);
 		
 	}
 
