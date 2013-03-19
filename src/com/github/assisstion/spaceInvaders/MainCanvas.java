f7e87981e7444cb5002da4cc391c6b2f034c5b8b
Markus Feng <markusfeng@gmail.com>
2013-03-19 21:53:42 +0800

    Changed main class to ensure thread saftely with Swing
    Also updated gitignore to exclude eclipse settings files

--- src/com/github/assisstion/spaceInvaders/MainCanvas.java
+++ src/com/github/assisstion/spaceInvaders/MainCanvas.java
@@ -3,6 +3,7 @@ package com.github.assisstion.spaceInvaders;
 import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import com.github.assisstion.spaceInvaders.menu.MainMenuBuilder;
 import com.github.assisstion.spaceInvaders.menu.Menu;
@@ -36,52 +37,60 @@ public final class MainCanvas {
 	}
 	
 	public static void main(String[] args) {
-		try {
-			if(System.getProperty("os.name").equalsIgnoreCase("Mac OS X")){
-				// take the menu bar off the jframe
-				System.setProperty("apple.laf.useScreenMenuBar", "true");
-				
-				// set the name of the application menu item
-				System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Space Invaders");
-				
-			}
-			
-			/*
-			 * Create a new JFrame and set it's properties up.
-			 */
-			
-			System.out.println("Program launch");
-			frame = new JFrame("Space Invaders");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setResizable(false);
-			 
-			/*
-			 * Creates the engine and adds it to the frame
-			 */
-			
-			System.out.println("Engine created");
-			
-			/*
-			 * Pack the frame, position it in the center of the screen, and then display
-			 * it, and add the menu.
-			 */
-			synchronized(audioLock){
-				menu = new Menu();
-				
-				frame.setContentPane(menu);
-				
-				frame.setLocationRelativeTo(null);
-				frame.pack();
-				frame.setVisible(true);
-				frame.validate();
-				menu.addMenuBuilder(new MainMenuBuilder());
-			}
+		SwingUtilities.invokeLater(new MainRunnable());
+	}
 	
-			System.out.println("Frame created");
-		}
-		catch(Exception e){
-			//TODO placeholder
-			e.printStackTrace();
+	private static class MainRunnable implements Runnable{
+		
+		@Override
+		public void run(){
+			try {
+				if(System.getProperty("os.name").equalsIgnoreCase("Mac OS X")){
+					// take the menu bar off the jframe
+					System.setProperty("apple.laf.useScreenMenuBar", "true");
+					
+					// set the name of the application menu item
+					System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Space Invaders");
+					
+				}
+				
+				/*
+				 * Create a new JFrame and set it's properties up.
+				 */
+				
+				System.out.println("Program launch");
+				frame = new JFrame("Space Invaders");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setResizable(false);
+				 
+				/*
+				 * Creates the engine and adds it to the frame
+				 */
+				
+				System.out.println("Engine created");
+				
+				/*
+				 * Pack the frame, position it in the center of the screen, and then display
+				 * it, and add the menu.
+				 */
+				synchronized(audioLock){
+					menu = new Menu();
+					
+					frame.setContentPane(menu);
+					
+					frame.setLocationRelativeTo(null);
+					frame.pack();
+					frame.setVisible(true);
+					frame.validate();
+					menu.addMenuBuilder(new MainMenuBuilder());
+				}
+		
+				System.out.println("Frame created");
+			}
+			catch(Exception e){
+				//TODO placeholder
+				e.printStackTrace();
+			}
 		}
 	}
 }
